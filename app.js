3f80491f27771d49867ef095c2fc3626020fddc5
maytega <maytega@uniovi.es>
2025-05-11 00:59:40 +0200

    post /permisos

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let express = require("express")
 let app = express()
 let port = 8081
 
+app.use(express.json())
+
 let routerpermissions = require("./routers/routerpermissions")
 
 app.use("/permisos", routerpermissions)
--- routers/routerpermissions.js
+++ routers/routerpermissions.js
@@ -5,4 +5,18 @@ let permissions = require("../data/permissions")
 
 routerpermissions.get("/", (req, res) => {res.json(permissions)})
 
+routerpermissions.post("/", (req, res) => {
+    let text = req.body.text
+    let userid = req.body.userId
+
+    let lastid = permissions[permissions.length-1].id
+    permissions.push({
+        id:lastid,
+        text:text,
+        approbedBy:[],
+        userId:userid
+    })
+    res.json({id:lastid+1})
+})
+
 module.exports = routerpermissions
